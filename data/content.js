7a861071e7e98c15b47615871251b7a31e100449
Sara Lichtarge <slichtarge@wustl.edu>
2026-08-05 14:34:16 -0700

    updating UAW leaders

--- data/content.js
+++ data/content.js
@@ -80,7 +80,7 @@ window.ORIENTATION = {
     { date:"2026-09-14", start:"10:00", end:"11:00", track:"resources",
       title:"OCPD Orientation", location:"Mission Hall 1105", speaker:"Kyla Foster", desc:"" },
     { date:"2026-09-08", start:"15:00", end:"16:00", track:"resources",
-      title:"UAW 4811", location:"Byers Hall 215", speaker:"Valeria Sanchez Estrada, Faye Orcales", desc:"" },
+      title:"UAW 4811", location:"Byers Hall 215", speaker:"Alex Ho, Faye Orcales", desc:"" },
     { date:"2026-09-15", start:"09:00", end:"10:00", track:"resources",
       title:"SMHW Orientation", location:"Mission Hall 1105", speaker:"Dr. Nicole Reinosa", desc:"" },
 
